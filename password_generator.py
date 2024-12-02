262c4ddc7c85b4478013e5b762ab52c93840644a
jonathanliu72 <jonathan.liu72@gmail.com>
2024-12-02 00:21:37 -0500

    added omit function

--- password_generator.py
+++ password_generator.py
@@ -6,38 +6,68 @@ from string import ascii_uppercase, ascii_lowercase, digits, punctuation
 from random import choices, shuffle
 from math import floor
 
-def password_generator(length:int)->str:
+def password_generator()->str:
+
+    while True:
+        length = input("Enter desired length of password: ")
+        if length.isdigit():
+            length = int(length)
+            break
+        else:
+            print("Please input an integer.")
+            continue
+    
+    exclusions = []
+    while True:
+        exclude = input("Enter characters, numbers, or symbols you would like to exlude ('quit' to quit): ")
+        if exclude == 'quit':
+            break
+        else:
+            if len(exclude) > 1:
+                print("Please enter one at a time.")
+                continue
+            exclusions.append(exclude)
+    
 
     password_list = []
-    password_list.extend(generate_uppercase(length))
-    password_list.extend(generate_lowercase(length))
-    password_list.extend(generate_number(length))
-    password_list.extend(generate_special_chars(length))
+    password_list.extend(generate_uppercase(length, exclusions))
+    password_list.extend(generate_lowercase(length, exclusions))
+    password_list.extend(generate_number(length, exclusions))
+    password_list.extend(generate_special_chars(length, exclusions))
     shuffle(password_list)
     password = ''.join(password_list)
 
     return password
 
 #Generates uppercase letters as a list
-def generate_uppercase(length):
+def generate_uppercase(length, exclusions):
     uppercase_letters = choices(ascii_uppercase, k = floor(length/4))
-    return uppercase_letters
+    return omit(uppercase_letters, exclusions)
 
 #Generates lowercase letters as a list
-def generate_lowercase(length):
+def generate_lowercase(length, exclusions):
     lowercase_letters = choices(ascii_lowercase, k = floor(length/4))
-    return lowercase_letters
+    return omit(lowercase_letters, exclusions)
 
 #Generates numbers as a list
-def generate_number(length):
+def generate_number(length, exclusions):
     numbers = choices(digits, k = floor(length/4))
-    return numbers
+    return omit(numbers, exclusions)
 
 #Generates special characters as a list
-def generate_special_chars(length):
-    special_chars = choices(punctuation, k= length - 3 * floor(length/4))
-    return special_chars
+def generate_special_chars(length, exclusions):
+    special_chars = choices(punctuation, k = length - 3 * floor(length/4))
+    return omit(special_chars, exclusions)
+    
+
+#Omits characters
+def omit(pswd_segment, exclusions):
+    while True:
+        for char in exclusions:
+            if char in pswd_segment:
+                continue
+        break
+    return pswd_segment
 
 #Test case
-print(password_generator(10))
-print("Password generation complete!")
+print(password_generator())
